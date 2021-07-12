9da0b1a6336d0fa66ad5f00fab7ce8f1e9653284
ulr0 <ulr0xb@gmail.com>
2021-07-12 21:36:46 +0900

    callback

---
+++ async/callback.js
@@ -0,0 +1,68 @@
+'use strict';
+
+// JavaScript is synchronous.
+// Execute the code block in order after hoisting.
+// hoisting: var, function declaration 
+console.log(1);
+setTimeout(() => console.log(2), 1000);
+console.log(3);
+
+// Synchronous callback
+function printImmediately(print) {
+    print();
+}
+printImmediately(() => console.log('hello'));
+
+// Asynchronous callback
+function printWithDelay(print, timeout) {
+    setTimeout(print, timeout);
+}
+printWithDelay(() => console.log('async callback'), 2000);
+
+// Callback Hell example
+class Userstorage {
+    loginUser(id, password, onSucecss, onError) {
+        setTimeout(() => {
+            if (
+                (id === 'ellie' && password === 'dream') ||
+                (id === 'coder' && password === 'academy')
+            ) {
+                onSucecss(id);
+            } else {
+                onError(new Error('not found'));
+            }
+        }, 2000);
+    }
+    
+    getRoles(user, onSucecss, onError) {
+        setTimeout(() => {
+            if (user === 'ellie') {
+                onSucecss({ name: 'ellie', role: 'admin' });
+            } else {
+                onError(new Error('no access'));
+            }
+        }, 1000);
+    }
+}
+
+const userStorage = new Userstorage();
+const id = prompt('enter your id');
+const password = prompt('enter your password');
+userStorage.loginUser(
+    id, 
+    password, 
+    user => {
+        userStorage.getRoles(
+            user, 
+            userWithRole => {
+                alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`);
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    }, 
+    error => {
+        console.log(error);
+    }
+);
