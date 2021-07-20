3193dce1d45fc329bd012e219a0f74851e0a08c9
ulr0 <ulr0xb@gmail.com>
2021-07-20 21:19:17 +0900

    promise

--- async/promise.js
+++ async/promise.js
@@ -5,8 +5,62 @@
 // Producer vs Consumer
 
 // 1. Producer
+// when new Promise is created, the executor runs automatically.
 const promise = new Promise((resolve, reject) => {
     // doing some heavy work (network, read files)
     console.log('doing something...');
+    setTimeout(() => {
+        resolve('ellie');
+        // reject(new Error('no network'));
+    }, 2000);
 });
 
+// 2. Consumers: then, catch, finally
+promise //
+    .then((value) => {
+        console.log(value);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+    .finally(() => {
+        console.log('finally');
+    });
+
+// 3. Promise chaining
+const fetchNumber = new Promise((resolve, reject) => {
+    setTimeout(() => resolve(1), 1000);
+});
+
+fetchNumber
+    .then(num => num * 2)
+    .then(num => num * 3)
+    .then(num => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => resolve(num-1), 1000);
+        });
+    })
+    .then(num => console.log(num));
+
+// 4. Error Handling
+const getHen = () => 
+    new Promise((resolve, reject)=> {
+        setTimeout(() => resolve('chicken'), 1000);
+    });
+const getEgg = hen => 
+    new Promise((resolve, reject) => {
+        setTimeout(() => reject(new Error(`error! ${hen} => egg`)), 1000);
+    });
+const cook = egg =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => resolve(`${egg} => fried egg`), 1000);
+    });
+
+getHen()
+    .then(hen => getEgg(hen)) // .then(getEgg)
+    .catch(error => {
+        return 'bread';
+    }) 
+    .then(egg => cook(egg)) // .then(cook)
+    .then(meal => console.log(meal)) // .then(console.log)
+    .catch(console.log); 
